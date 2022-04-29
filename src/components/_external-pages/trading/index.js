265ab2382310240b666ba9659d56b0182a4b6b1a
deliteser112 <74626440+deliteser112@users.noreply.github.com>
2022-04-29 22:43:24 +0900

    desktop version is done

--- src/components/_external-pages/trading/index.js
+++ src/components/_external-pages/trading/index.js
@@ -1,3 +1,9 @@
 export { default as LongShort } from './LongShort';
 export { default as Chart } from './Chart';
 export { default as CryptoForex } from './CryptoForex';
+export { default as OpenTradeOrders } from './OpenTradeOrders';
+export { default as LeverageCard } from './LeverageCard';
+export { default as AllTrades } from './AllTrades';
+export { default as Leaderboard } from './Leaderboard';
+export { default as PersonalTrading } from './PersonalTrading';
+export { default as NFTTrade } from './NFTTrade';
--- src/pages/TradingPage.js
+++ src/pages/TradingPage.js
@@ -1,10 +1,19 @@
 import React from 'react';
 // material
-import { Container, Box, Grid } from '@material-ui/core';
+import { Container, Box, Grid, Typography } from '@material-ui/core';
 import { useTheme } from '@material-ui/core/styles';
 // components
 import Page from '../components/Page';
-import { Chart, LongShort } from '../components/_external-pages/trading';
+import {
+  Chart,
+  LongShort,
+  OpenTradeOrders,
+  LeverageCard,
+  AllTrades,
+  Leaderboard,
+  PersonalTrading,
+  NFTTrade
+} from '../components/_external-pages/trading';
 // ----------------------------------------------------------------------
 
 export default function TradingPage() {
@@ -23,6 +32,52 @@ export default function TradingPage() {
             <LongShort />
           </Grid>
         </Grid>
+        <Box m={3} />
+        <Grid container spacing={2}>
+          <Grid item xs={12} lg={8} xl={9}>
+            <OpenTradeOrders title="Your Open Trades" content="Collaterals, profits, losses, are in DAI" />
+          </Grid>
+          <Grid item xs={12} lg={4} xl={3}>
+            <OpenTradeOrders title="Your Open Orders" content="Can be canceled after 1 minute." />
+          </Grid>
+        </Grid>
+      </Container>
+      <Box m={15} />
+      <Container>
+        <Typography variant="h2" sx={{ textAlign: 'center' }}>
+          VIRTUAL LEVERAGED TRADING
+        </Typography>
+        <Typography variant="body1" sx={{ textAlign: 'center' }}>
+          Claim your free 10,000 DAI and train your leveraged trading skills on our platform.
+        </Typography>
+        <Box m={7} />
+        <Grid container spacing={2}>
+          <Grid item xs={12} lg={6}>
+            <LeverageCard value="$2,579,157" subValue="DAILY VOLUME" />
+          </Grid>
+          <Grid item xs={12} lg={6}>
+            <LeverageCard value="63" subValue="TRADES COUNT" />
+          </Grid>
+        </Grid>
+        <Box m={3} />
+        <Typography variant="body1" sx={{ textAlign: 'center' }}>
+          Copy referral link Traders earn 0.01% more, and you earn 0.01% on each trade.You have earned ... GNS so far.
+        </Typography>
+        <Box m={15} />
+        <Grid container spacing={2}>
+          <Grid item xs={12} lg={6}>
+            <AllTrades title="All Trades (24h)" />
+          </Grid>
+          <Grid item xs={12} lg={6}>
+            <Leaderboard title="Leaderboard (7 days)" />
+            <Box m={2} />
+            <PersonalTrading title="Personal Trading Report" />
+          </Grid>
+        </Grid>
+      </Container>
+      <Box m={6} />
+      <Container maxWidth="xl">
+        <NFTTrade title="NFT Orders" content="There is a LINK fee for each NFT order." />
       </Container>
     </Page>
   );
